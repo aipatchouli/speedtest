89b32081e8822a8627ad40b802a0142d7e03a4ff
aipatchouli <48445594+aipatchouli@users.noreply.github.com>
2020-02-08 16:31:50 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -11,22 +11,11 @@ geekbench4() {
 	GEEKBENCH_URL=$(echo -e $GEEKBENCH_TEST | head -1)
 	GEEKBENCH_URL_CLAIM=$(echo $GEEKBENCH_URL | awk '{ print $2 }')
 	GEEKBENCH_URL=$(echo $GEEKBENCH_URL | awk '{ print $1 }')
-	sleep 10
+	sleep 1
 	GEEKBENCH_SCORES=$(curl -s $GEEKBENCH_URL | grep "class='score' rowspan")
 	GEEKBENCH_SCORES_SINGLE=$(echo $GEEKBENCH_SCORES | awk -v FS="(>|<)" '{ print $3 }')
 	GEEKBENCH_SCORES_MULTI=$(echo $GEEKBENCH_SCORES | awk -v FS="(<|>)" '{ print $7 }')
 	
-	if [[ $GEEKBENCH_SCORES_SINGLE -le 1700 ]]; then
-		grank="(POOR)"
-	elif [[ $GEEKBENCH_SCORES_SINGLE -ge 1700 && $GEEKBENCH_SCORES_SINGLE -le 2300 ]]; then
-		grank="(FAIR)"
-	elif [[ $GEEKBENCH_SCORES_SINGLE -ge 2300 && $GEEKBENCH_SCORES_SINGLE -le 3000 ]]; then
-		grank="(GOOD)"
-	elif [[ $GEEKBENCH_SCORES_SINGLE -ge 3000 && $GEEKBENCH_SCORES_SINGLE -le 4000 ]]; then
-		grank="(VERY GOOD)"
-	else
-		grank="(EXCELLENT)"
-	fi
 	
 	echo -ne "\e[1A"; echo -ne "\033[0K\r"
 	
@@ -39,26 +28,3 @@ geekbench4() {
 }
 
 
-log="$HOME/speedtest.log"
-true > $log
-
-case $1 in
-	
-   	'gb'|'-gb'|'--gb'|'geek'|'-geek'|'--geek' )
-		geekbench4;;
-	
-esac
-
-
-
-if [[  ! $is_share == "share" ]]; then
-	case $2 in
-		'share'|'-s'|'--s'|'-share'|'--share' )
-			if [[ $3 == '' ]]; then
-				sharetest ubuntu;
-			else
-				sharetest $3;
-			fi
-			;;
-	esac
-fi
