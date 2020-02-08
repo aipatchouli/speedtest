4e4f7bcf2703c67f738bf3439bd14211dfe5a8f0
aipatchouli <48445594+aipatchouli@users.noreply.github.com>
2020-02-08 17:43:41 +0800

    Update gb4.sh

--- gb4.sh
+++ gb4.sh
@@ -64,7 +64,7 @@ geekbench4() {
 	GEEKBENCH_SCORES_MULTI=$(echo $GEEKBENCH_SCORES | awk -v FS="(<|>)" '{ print $7 }')
 	
 	echo -ne "\e[1A"; echo -ne "\033[0K\r"
-	
+	echo " Geekbench v4 CPU Benchmark:"
 	echo "" | tee -a $log
 	echo -e "  Single Core : $GEEKBENCH_SCORES_SINGLE  $grank" | tee -a $log
 	echo -e "   Multi Core : $GEEKBENCH_SCORES_MULTI" | tee -a $log
